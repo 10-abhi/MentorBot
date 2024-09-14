b4edd3f6a81e9e78961f67be82ae0eba6ab56964
10-abhi <abhishek81936@gmail.com>
2024-09-14 18:44:51 +0530

    Updating Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Homepage } from './pages/Homepage'
-
+import { Signin } from "./pages/Signin"
+import { Signup } from './pages/Signup'
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-   <Homepage></Homepage>
-    </>
+    <div>
+       <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Homepage></Homepage>}> </Route>
+          <Route path="signup" element={<Signup />}></Route>
+          <Route path="signin" element={<Signin></Signin>}></Route>
+          {/* <Route path="/dashboard" element={<Dashboard />}> </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
